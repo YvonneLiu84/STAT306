4cf2311f9f3b5d07353f578914e84c7cfdd051ad
Yvonne <yvonne@dhcp-206-87-223-147.ubcsecure.wireless.ubc.ca>
2017-03-28 11:47:51 -0700

    add plot cont AQI

--- AQI_Cont.R
+++ AQI_Cont.R
@@ -4,3 +4,5 @@ library(readxl)
 X306 <- read_excel("~/Desktop/STAT306_lab9/proj/306.xlsx")
 
 fit_con<-lm(X306$AQI~X306$CO_24h+X306$NO2_24h+X306$O3_24h+X306$PM10_24h+X306$PM2.5_24h+X306$SO2_24h)
+
+plot(X306$AQI, X306$PM10_24h)
