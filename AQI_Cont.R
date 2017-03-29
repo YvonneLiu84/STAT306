49376170d4f4ae5502e32fd29f5690d5a927beea
Shiyue Ma <Joy@dhcp-128-189-223-44.ubcsecure.wireless.ubc.ca>
2017-03-28 18:35:39 -0700

    add original dataset

--- AQI_Cont.R
+++ AQI_Cont.R
@@ -10,3 +10,16 @@ plot(X306$AQI~X306$PM2.5_24h)
 plot(X306$AQI~X306$SO2_24h)
 
 fit_con<-lm(X306$AQI~X306$CO_24h+X306$NO2_24h+X306$O3_24h+X306$PM10_24h+X306$PM2.5_24h+X306$SO2_24h)
+
+sumFit <- summary(fit_con)
+sigma <- sumFit$sigma
+pred<-predict(fit_con)
+res<-resid(fit_con) 
+qqnorm(res,main="normal Q-Q plot of residuals")
+qqline(res)
+plot(pred,res,xlab="predicted value",ylab="residual")
+abline(h=2*sigma); abline(h=-2*sigma); abline(h=0)
+title("const spread or pattern as yhat increases?",cex.main=0.8)
+
+ls.diag(fit_con)
+
